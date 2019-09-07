be715a43e075f299828c2cfb7a728ffb38cc19d7
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-09-07 15:47:17 +0530

    Spotify(ied)

--- smartwatch.js
+++ smartwatch.js
@@ -5,7 +5,7 @@ function screen(){
     document.getElementById('notificationBar').style.visibility = "visible";
     document.getElementById('notificationBar').innerHTML  = "Menu";
     document.getElementById('time').style.visibility = "visible";
-    // document.getElementsByClassName('goHome').style.display = 'block';
+    document.getElementById('spotify').style.display  = "none";
     document.getElementById('goHome').style.display = 'block';
     document.getElementById("messageScreen").style.display = "none";
     initialDisplay();
@@ -17,6 +17,9 @@ function initialDisplay(){
     let dayArray =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let dayInStr = dayArray[dayInNum];
     let h = date1.getHours();
+    let y = date1.getFullYear();
+    let mo = date1.getMonth();
+    let d = date1.getDate();
     
     let meridiem = 'am'
     if(h >= 13)
@@ -37,20 +40,30 @@ function initialDisplay(){
     console.log(dayInStr +" "+h+":"+m+" "+meridiem)
     // document.getElementById('date')[0].innerHTML = dayInStr +" "+h+":"+m+":"+s+" "+meridiem;
     document.getElementById("time").innerHTML = h+":"+m+" "+meridiem;
+    document.getElementById("dispTime").innerHTML= "<b>"+h+":"+m+"</b>"+meridiem+"<br><p id='salutation'>"+dayInStr+", " +d+"-"+mo+"-"+"19"+"</p>";
+    console.log("<b>"+h+":"+m+"</b>"+meridiem+"<br><p>"+dayInStr+", " +d+"-"+mo+"-"+y+"</p>");
+    console.log(dayInStr +" "+h+":"+m+" "+meridiem)
 }
 function message(){
     document.getElementById('iconPage').style.display="none";
     document.getElementById("messageScreen").style.display = "flex";
-    document.getElementsByClassName("myMessageFlex").style.display = "flex";
+    // document.getElementsByClassName("myMessageFlex").style.display = "flex";
+    console.log('here');
+    document.getElementById('notificationBar').innerHTML = "Messages";
 
 }
 function music(){
     document.getElementById('iconPage').style.display="none";
+    document.getElementById('notificationBar').innerHTML  = "Spotify";
+    document.getElementById('spotify').style.display  = "flex";
     // document.getElementById("messageScreen").style.display = "flex";
     // document.getElementsByClassName("myMessageFlex").style.display = "flex";
 }
 function timer(){
     document.getElementById('iconPage').style.display="none";
+    document.getElementById('notificationBar').innerHTML  = "Timer";
     // document.getElementById("messageScreen").style.display = "flex";
     // document.getElementsByClassName("myMessageFlex").style.display = "flex";
 }
+
+
